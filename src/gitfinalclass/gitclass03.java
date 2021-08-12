696392a172e9fec865b9505f24fcf914b99cdbbd
MikeLSDET <mikesdetlopez@gmail.com>
2021-08-11 21:23:57 -0400

    third commit

--- src/gitfinalclass/gitclass03.java
+++ src/gitfinalclass/gitclass03.java
@@ -3,7 +3,7 @@ package gitfinalclass;
 public class gitclass03 {
 
 	public static void main(String[] args) {
-		System.out.println("This is the first commit file");
+		System.out.println("This is the none commit file");
 
 	}
 
